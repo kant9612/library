180bb32ec2ed39dfc0138e85880fe0c6c017e713
髙山寛 <kanstephentakayama@gmail.com>
2024-10-25 00:04:48 +0900

    <FIX>214B

--- 214/B/Main.java
+++ 214/B/Main.java
@@ -12,14 +12,15 @@ public class Main {
     int ans = 0;
 
     for (int a = 0; a <= S; a++) {
-      for (int b = 0; a <= S; b++) {
-        for (int c = 0; a <= S; b++) {
+      for (int b = 0; b <= S; b++) {
+        for (int c = 0; c <= S; c++) {
           if (a + b + c <= S && a * b * c <= T)
             ans++;
         }
       }
     }
 
+    System.out.println(ans);
     // Scannerを閉じる
     sc.close();
   }
