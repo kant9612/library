52e8f693f9233e79370576c951c0efa732da0b5e
髙山寛 <kanstephentakayama@gmail.com>
2024-11-04 23:57:43 +0900

    <ADD>227A;

--- 227/A/Main.java
+++ 227/A/Main.java
@@ -1,5 +1,21 @@
-package 227.A;
+import java.util.*;
 
 public class Main {
-  
+  public static void main(String[] args) {
+    // Scannerオブジェクトの作成
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    int K = sc.nextInt();
+    int A = sc.nextInt();
+    int ans = 0;
+
+    ans = (A + K - 1) % N;
+    if (ans == 0)
+      ans = N;
+    System.out.println(ans);
+
+    // Scannerを閉じる
+    sc.close();
+  }
 }
