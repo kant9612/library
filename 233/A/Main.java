c3de688cf87e8fa9004af1b1bd01cc63499465e9
髙山寛 <kanstephentakayama@gmail.com>
2024-11-09 17:02:17 +0900

    <ADD>233A

---
+++ 233/A/Main.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+
+  public static int ceil(int a, int b) {
+    return (a + b - 1) / b;
+  }
+
+  public static void main(String[] args) {
+    // Scannerオブジェクトの作成
+    Scanner sc = new Scanner(System.in);
+
+    int x = sc.nextInt();
+    int y = sc.nextInt();
+    int ans = 0;
+
+    if (x >= y) {
+      ans = 0;
+    } else {
+      ans = ceil(y - x, 10);
+    }
+
+    System.out.println(ans);
+
+    // Scannerを閉じる
+    sc.close();
+  }
+}
