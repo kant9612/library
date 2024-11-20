fd6362856315624439bbe15f5e7c9be3508158bf
髙山寛 <kanstephentakayama@gmail.com>
2024-11-20 23:55:59 +0900

    <ADD>Saruman's Army

---
+++ library/Saruman's Army/Main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAX_N = 1000; // 最大文字列長の設定
+
+// 入力
+int N, R;
+int X[MAX_N];
+
+int main() {
+    // 入力の読み込み
+    cin >> N >> R;
+    cin >> X;
+    
+    // 結果を出力
+    cout << ans << endl;
+
+    return 0;
+}
