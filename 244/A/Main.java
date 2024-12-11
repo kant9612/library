191dfc0d702ca2f094ffd41c30ca14197be92843
髙山寛 <kanstephentakayama@gmail.com>
2024-12-11 23:43:52 +0900

    <ADD>244A

--- 244/A/Main.java
+++ 244/A/Main.java
@@ -1,5 +1,15 @@
-package 244.A;
+import java.util.Scanner;
 
 public class Main {
-  
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    sc.nextLine(); // 改行を消費
+    String s = sc.nextLine();
+
+    char ans = s.charAt(n - 1);
+    System.out.println(ans);
+
+    sc.close();
+  }
 }
