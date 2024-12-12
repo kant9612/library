310422d29ce89df4c2591a0d7af887efc83ac59e
髙山寛 <kanstephentakayama@gmail.com>
2024-12-13 00:02:01 +0900

    <ADD>246A

--- 246/A/Main.java
+++ 246/A/Main.java
@@ -1,5 +1,37 @@
-package 246.A;
+import java.util.Scanner;
 
 public class Main {
-  
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int c = sc.nextInt();
+    int d = sc.nextInt();
+    int e = sc.nextInt();
+    int f = sc.nextInt();
+    int x;
+    int y;
+    String ans;
+
+    if (a == c) {
+      x = e;
+    } else if (c == e) {
+      x = a;
+    } else {
+      x = c;
+    }
+
+    if (b == d) {
+      y = f;
+    } else if (d == f) {
+      y = b;
+    } else {
+      y = d;
+    }
+
+    ans = x + " " + y;
+    System.out.println(ans);
+
+    sc.close();
+  }
 }
