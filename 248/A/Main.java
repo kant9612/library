dfd6b19a09ba4c9fc43fbf72e43e244bfe4b5443
髙山寛 <kanstephentakayama@gmail.com>
2024-12-16 00:07:12 +0900

    <FIX>248A

--- 248/A/Main.java
+++ 248/A/Main.java
@@ -1,28 +1,27 @@
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    String s = sc.nextLine();
-    // 1. 文字列を char 配列に変換
-    char[] charArray = s.toCharArray();
+    String S = sc.nextLine(); // 数字を入力
+    boolean[] flag = new boolean[10]; // 10桁分のフラグ配列を用意
 
-    // 2. 配列をソート
-    Arrays.sort(charArray);
-
-    // 3. ソート済み配列を新しい文字列に変換
-    String sortedString = new String(charArray);
-    int ans = 0;
-
-    for (int i = 0; i <= s.length(); i++) {
-      if (sortedString.charAt(i) != (char) ('0' + i)) {
-        ans = i;
-        break;
-      }
+    // すべてのフラグを true に初期化
+    for (int i = 0; i < 10; i++) {
+      flag[i] = true;
     }
 
-    System.out.println(ans);
+    // 入力された9桁の数字を解析し、該当する桁を false に設定
+    for (int i = 0; i < S.length(); i++) {
+      flag[S.charAt(i) - '0'] = false; // '0' を引くことで整数化
+    }
+
+    // フラグが true のインデックス（欠けている数字）を出力
+    for (int i = 0; i < 10; i++) {
+      if (flag[i]) {
+        System.out.println(i);
+      }
+    }
 
     sc.close();
   }
