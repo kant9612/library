6ef6e51e0ff6523eb961824bbb66dabee4bd5352
髙山寛 <kanstephentakayama@gmail.com>
2025-03-08 18:35:09 +0900

    <ADD>DPフィボナッチ数列

---
+++ ライブラリ/DP_1/Main.java
@@ -0,0 +1,31 @@
+// フィボナッチ数列
+package ライブラリ.DP_1;
+
+import java.util.Scanner;
+
+import javax.print.DocPrintJob;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+
+    // DP配列の作成
+    int[] fib = new int[n + 1];
+
+    // 初期条件
+    fib[0] = 1;
+    if (n > 0) {
+      fib[1] = 1;
+    }
+
+    // フィボナッチ数列の計算
+    for (int i = 2; i <= n; i++) {
+      fib[i] = fib[i - 1] + fib[i - 2];
+    }
+
+    System.out.println(fib[n]);
+
+  }
+}
